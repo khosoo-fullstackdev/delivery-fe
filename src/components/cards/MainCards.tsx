62b1ca9217084f37ce3cc7ec225c5e53ea296e0d
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-03-27 12:38:38 +0800

    asd

--- src/components/cards/MainCards.tsx
+++ src/components/cards/MainCards.tsx
@@ -1,34 +1,31 @@
-import { StarIcon } from "@/icons/StarIcon";
-import { Box, Stack, Typography } from "@mui/material";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+// import { StarIcon } from "@/icons/StarIcon";
 
 export const MainDishCard = () => {
   return (
-    <Stack width="1200px" height="344px">
-      <Stack direction="row" alignItems="center" justifyContent="space-between">
-        <Stack direction="row" alignItems="center" justifyContent="center">
-          <StarIcon />
-          <Typography fontSize="22px" fontWeight="700">
-            Амттан
-          </Typography>
-        </Stack>
-        <Typography textAlign="center" color="#18BA51">
-          Бүгдийг харах {">"}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography>Lizard</Typography>
+        <Typography>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
         </Typography>
-      </Stack>
-      <Stack>
-        <Stack>
-          <Box
-            width="282px"
-            height="186px"
-            borderRadius="16px"
-            boxShadow="0px 3px 6px -2px rgba(0, 0, 0, 0.10), 0px 6px 10px 0px rgba(0, 0, 0, 0.07)"
-            sx={{ backgroundImage: "URL('/Home/cardsPic/ice-cream.svg'" }}
-          ></Box>
-          <Typography fontSize="18px" fontWeight="800">
-            Өглөөний хоол
-          </Typography>
-        </Stack>
-      </Stack>
-    </Stack>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 };
